b39ee0492c687fa6e569b2473353d8147a926fea
Mamun-Ur-Rashid <engmamun656@gmail.com>
2023-04-11 11:00:23 +0600

    pie chart added

--- src/component/Statistices.jsx
+++ src/component/Statistices.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Pie, PieChart, Tooltip } from 'recharts';
 
 const Statistices = () => {
+    const data = [
+    {name : "assignment-1", value: 57},
+    {name : "assignment-2", value: 57},
+    {name : "assignment-3", value: 60},
+    {name : "assignment-4", value: 60},
+    {name : "assignment-5", value: 60},
+    {name : "assignment-6", value: 60},
+    {name : "assignment-7", value: 60},
+    {name : "assignment-8", value: 58},
+    ]
     return (
-        <div>
-            <h1>Statistices page</h1>
+        <div className='container text-center mx-auto justify-start flex-col items-center w-3/4'>
+            <h1 className='text-2xl text-blue-500 font-semibold underline'>Assignment Marks</h1>
+            <PieChart width={800} height={800}>
+                <Pie 
+                datakey ="vale"
+                isAnimationActive = {false}
+                data = {data}
+                cx={300}
+                cy={300}
+                outerRadius={200}
+                fill = "#847FCC"
+                label>
+                </Pie>
+                <Tooltip />
+            </PieChart>
         </div>
     );
 };
